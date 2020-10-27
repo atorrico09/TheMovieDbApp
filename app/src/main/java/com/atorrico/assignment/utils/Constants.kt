18efe1b6576799d976a8a1e53134e68f834cb623
atorrico <atorrico@lanacion.com.ar>
2020-10-27 17:50:29 -0300

    Hide API_KEY in BuildConfigs

--- app/src/main/java/com/atorrico/assignment/utils/Constants.kt
+++ app/src/main/java/com/atorrico/assignment/utils/Constants.kt
@@ -1,5 +1,8 @@
 package com.atorrico.assignment.utils
 
+import com.atorrico.assignment.BuildConfig
+
 object Constants {
+    const val API_KEY_MOVIE_DB_APP = BuildConfig.MOVIE_DB_API_KEY
     const val BASE_URL_IMAGES = "https://image.tmdb.org/t/p/w500"
 }
